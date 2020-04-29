43a1a4508fe81d081de803a7098a3acd9987ff2a
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-28 23:29:56 -0400

    chr_str reading

---
+++ chr_str.py
@@ -0,0 +1,14 @@
+#Darshan Mistry...
+#character and number data auto finder....
+
+
+user_name = "darshan"
+user_surname = "mistry"
+mail = "gmail"
+age = 24
+name = user_name +" " + user_surname
+print("%s is %d years old." % (name,age))
+
+email_id = user_name + user_surname + "@" + mail + ".com"
+
+print(email_id)
