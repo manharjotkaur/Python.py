d8590d3ccac005a1dc3088188ce57be757f7b2ad
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-28 23:30:44 -0400

    Update chr_str.py

--- chr_str.py
+++ chr_str.py
@@ -1,13 +1,12 @@
 #Darshan Mistry...
 #character and number data auto finder....
 
-
 user_name = "darshan"
 user_surname = "mistry"
 mail = "gmail"
 age = 24
 name = user_name +" " + user_surname
-print("%s is %d years old." % (name,age))
+print("%s is %d years old." % (name,age)) #caharacter string and number string reading
 
 email_id = user_name + user_surname + "@" + mail + ".com"
 
