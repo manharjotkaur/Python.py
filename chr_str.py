624e8ff603ef1380788690290adcd8879375472d
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-30 22:32:04 -0400

    Update chr_str.py

--- chr_str.py
+++ chr_str.py
@@ -8,6 +8,6 @@ age = 24
 name = user_name +" " + user_surname
 print("%s is %d years old." % (name,age)) #caharacter string and number string reading
 
-email_id = user_name + user_surname + "@" + mail + ".com"
+ email_id = user_name + user_surname + "@" + mail + ".com"
 
 print(email_id)
