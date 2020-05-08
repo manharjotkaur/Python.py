6e1e222971ef0b5481ee1d8c1859eaf2e8556730
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-05-07 22:54:42 -0400

    Update NUM_DIG.py

--- NUM_DIG.py
+++ NUM_DIG.py
@@ -2,8 +2,10 @@
 #Number of Digit Finder.
 
 A=int(input("Please enter the number:"))
+
 count=0
 while(n>0):
     digit=digit+1
     n=n//10
-print("The total number of digits are:",digit)
+    
+print("The total number of digits are:",digit) #digit work as a function.... 
