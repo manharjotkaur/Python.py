05735749ce5c60931a541cfa0e8a74420ee4304c
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-05-16 23:44:11 -0400

    Update chr_str.py

--- chr_str.py
+++ chr_str.py
@@ -5,7 +5,7 @@ user_name = "darshan"
 user_surname = "mistry"
 mail = "gmail"
 age = 24
-name = user_name +"" + user_surname
+name = user_name +" " + user_surname
 print("%s is %d years old." % (name,age)) #caharacter string and number string reading
 
 email_id = user_name + user_surname + "@" + mail + ".com"
