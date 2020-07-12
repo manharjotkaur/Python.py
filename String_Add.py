b245831ba93f2cd77a5552aee074afa64d7ec7b9
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-07-11 23:57:53 -0400

    Update String_Add.py

--- String_Add.py
+++ String_Add.py
@@ -5,5 +5,4 @@ name="Darshan" #string created.
 surname="Mistry" #str_2
 birthyear="1995" #str_3
 fullname=surname+"@"+birthyear+name+".com" #string merging.
-
 print(fullname) #print the whole string with function.
